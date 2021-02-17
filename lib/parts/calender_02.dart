864b97598e9ebf54b38c69c03cbfbe4ba6b4a94b
yokokida <a.pirlo1109@gmail.com>
2021-02-18 00:08:18 +0900

    calender action

--- lib/parts/calender_02.dart
+++ lib/parts/calender_02.dart
@@ -9,32 +9,50 @@ class NewsPage extends StatefulWidget {
 
 class _NewsPageState extends State<NewsPage> {
   CalendarController _controller;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+  TextEditingController _eventController;
 
   @override
   void initState() {
     super.initState();
     _controller = CalendarController();
+    _eventController = TextEditingController();
+    _events = {};
+    _selectedEvents = [];
   }
+
+
+  final Map<DateTime, List> _holidays = {
+    DateTime(2021, 1, 1): ['謹賀新年'],
+    DateTime(2021, 1, 6): ['Epiphany'],
+    DateTime(2021, 2, 16): ['Valentine\'s Day'],
+    DateTime(2021, 4, 21): ['Easter Sunday'],
+    DateTime(2021, 4, 22): ['Easter Monday'],
+  };
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('お知らせ'),
+        title: Text('スケジュール'),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             TableCalendar(
+              holidays: _holidays,
               initialCalendarFormat: CalendarFormat.month,
               calendarStyle: CalendarStyle(
                   todayColor: Colors.blue,
                   selectedColor: Theme.of(context).primaryColor,
                   todayStyle: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                      color: Colors.white)
+                      fontSize: 24.0,
+                      color: Colors.white
+                  )
               ),
               headerStyle: HeaderStyle(
                 centerHeaderTitle: true,
@@ -43,13 +61,14 @@ class _NewsPageState extends State<NewsPage> {
                   borderRadius: BorderRadius.circular(22.0),
                 ),
                 formatButtonTextStyle: TextStyle(color: Colors.white),
-                formatButtonShowsNext: false,
+                formatButtonShowsNext: true,
               ),
-              startingDayOfWeek: StartingDayOfWeek.monday,
-              onDaySelected: (date, events,e) {
-                print(date.toUtc());
+              startingDayOfWeek: StartingDayOfWeek.sunday,
+              onDaySelected: (date, events, holidays) {
+                setState(() {
+                  _selectedEvents = events;
+                });
               },
-
               builders: CalendarBuilders(
                 selectedDayBuilder: (context, date, events) => Container(
                     margin: const EdgeInsets.all(5.0),
@@ -73,10 +92,62 @@ class _NewsPageState extends State<NewsPage> {
                     )),
               ),
               calendarController: _controller,
-            )
+            ),
+            ..._selectedEvents.map((event) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height/20,
+                width: MediaQuery.of(context).size.width/2,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey)
+                ),
+                child: Center(
+                    child: Text(event,
+                      style: TextStyle(color: Colors.blue,
+                          fontWeight: FontWeight.bold,fontSize: 16),)
+                ),
+              ),
+            )),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _showAddDialog,
+      ),
     );
+  }
+
+  _showAddDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("予定追加"),
+          content: TextField(
+            controller: _eventController,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("保存",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),),
+              onPressed: () {
+                if (_eventController.text.isEmpty) return;
+                setState(() {
+                  if (_events[_controller.selectedDay] != null) {
+                    _events[_controller.selectedDay]
+                        .add(_eventController.text);
+                  } else {
+                    _events[_controller.selectedDay] = [
+                      _eventController.text
+                    ];
+                  }
+                  _eventController.clear();
+                  Navigator.pop(context);
+                });
+              },
+            )
+          ],
+        ));
   }
 }
