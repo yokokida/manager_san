2620b4661b45fd2b6f66f8b74bb4e43ac1ebf45e
yokokida <a.pirlo1109@gmail.com>
2021-03-24 08:23:35 +0900

    syncfusion calenders

--- lib/calender/page_calender_syncfusion.dart
+++ lib/calender/page_calender_syncfusion.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:manager_san/calender/screen_add.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 class SyncCalender extends StatefulWidget {
   @override
@@ -11,13 +15,28 @@ class _SyncCalenderState extends State<SyncCalender> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('予定表'),
+        ),
         body: Container(
           child: SfCalendar(
             view: CalendarView.month,
+            showDatePickerButton: true,
+            allowViewNavigation: true,
             monthViewSettings: MonthViewSettings(
+              appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
               showAgenda: true,
             ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => AddEventPage02(
+                // chosenDay: _onDaySelected,
+              )
+              )
+          ), // onPressed: _showAddDialog,
         ),
       ),
     );
